8b7a0ff8497577e15aa0f5afa379852f111c1c39
Swamyrangareddy Muthumula <127617789+swamyrangareddy@users.noreply.github.com>
2025-01-19 14:59:38 +0530

    Add files via upload

---
+++ app22.py
@@ -0,0 +1,57 @@
+import streamlit as st
+
+# Set the page title
+st.set_page_config(page_title="Photo Gallery App")
+
+# Define the photos and titles
+photo_data = {
+    "Kobbari Bondam": [
+        {"image": "img/Kobbari Bondam/img 1.jpg", "title": "Design 1"},
+        {"image": "img/Kobbari Bondam/img 2.jpg", "title": "Design 2"},
+        {"image": "img/Kobbari Bondam/img 3.jpg", "title": "Design 3"},
+        {"image": "img/Kobbari Bondam/img 4.jpg", "title": "Design 4"},
+        {"image": "img/Kobbari Bondam/img 5.jpg", "title": "Design 5"},
+        {"image": "img/Kobbari Bondam/img 6.jpg", "title": "Design 6"},
+        {"image": "img/Kobbari Bondam/img 7.jpg", "title": "Design 7"},
+        {"image": "img/Kobbari Bondam/img 8.jpg", "title": "Design 8"},
+        {"image": "img/Kobbari Bondam/img 9.jpg", "title": "Design 9"},
+        {"image": "img/Kobbari Bondam/img 10.jpg", "title": "Design 10"},
+        {"image": "img/Kobbari Bondam/img 11.jpg", "title": "Design 11"},
+        {"image": "img/Kobbari Bondam/img12.jpg", "title": "Design 12"},
+        {"image": "img/Kobbari Bondam/img 13.jpg", "title": "Design 13"},
+        {"image": "img/Kobbari Bondam/img 14.jpg", "title": "Design 14"},
+        {"image": "img/Kobbari Bondam/img 15.jpg", "title": "Design 15"},
+        {"image": "img/Kobbari Bondam/img 16.jpg", "title": "Design 16"},
+        {"image": "img/Kobbari Bondam/img 17.jpg", "title": "Design 17"},
+        {"image": "img/Kobbari Bondam/img 18.jpg", "title": "Design 18"},
+        {"image": "img/Kobbari Bondam/img 19.jpg", "title": "Design 19"},
+        {"image": "img/Kobbari Bondam/img 20.jpg", "title": "Design 20"},
+    ],
+    "Adduthera": [
+        {"image": "img/Adduthera/img 1.jpg", "title": "Design 1"},
+        {"image": "img/Adduthera/img 2.jpg", "title": "Design 2"},
+        {"image": "img/Adduthera/img 3.jpg", "title": "Design 3"},
+        {"image": "img/Adduthera/img 4.jpg", "title": "Design 4"},
+        {"image": "img/Adduthera/img 5.jpg", "title": "Design 5"},
+    ]
+}
+
+# Create a sidebar for tabs
+selected_tab = st.sidebar.selectbox(
+    "Select a tab",
+    ["Kobbari Bondam", "Adduthera"]
+)
+
+# Display the selected tab's photos
+if selected_tab in photo_data:
+    st.title(selected_tab)
+    photos = photo_data[selected_tab]
+    cols = st.columns(2)
+    for idx, photo in enumerate(photos):
+        with cols[idx % 2]:
+            st.image(photo["image"],width=300)
+            st.write(photo["title"])
+            # horizontal line
+            st.markdown("---")
+else:
+    st.write("No photos available for this tab.")
